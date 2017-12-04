bb47ff3a569ebfaa041d5395877e48b1e9e333ab
addisonv <addisonv@umich.edu>
2017-12-04 13:21:16 -0500

    Commit

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-var = "I love SI206"
-print(var[2:4])
-print(var[-1])
+import facebook 
+import urllib3
+import requests
+
+token = "EAALj5ZCoLn6YBAMsuoJj5tDjgEihh6VZBFkxPY3bK8yIk6PfzV6MCSH7bYdfiZBLegWW5XBSa74TfiBsoH9JP7qAcetDZAYeEwlcJ8SxlNuFEofEMVAZBz1JpQHbmIzsE0z0DRKh1Y5bmByKvzQpfIPZA7TrQrGYLiwq62dvDlvAZDZD"
+graph = facebook.GraphAPI(access_token=token, version = 2.7)
+events = graph.request("/search?q=Poetry&type=event&limit=10000")
+eventList = events["data"]
+print(eventList)
